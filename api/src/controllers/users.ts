a53f221e9e5063d8a8b7f137b836bdeb2fe3dbd7
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-04-25 22:17:11 -0300

    feat: start login method

--- api/src/controllers/users.ts
+++ api/src/controllers/users.ts
@@ -1,5 +1,34 @@
+import "reflect-metadata"; 
+import {createConnection} from "typeorm";
+import {User} from "../entity/user"; //import User entity
+
 export default class UsersController {
     public login(req, res) {
-        return res.status(200).json("Hello00o, World!");
+        createConnection().then(async connection => { 
+
+            console.log("Inserting a new record into the student database..."); 
+            
+            //create student object 
+            const user = new User(); 
+            
+            //Assign student name and age here 
+            user.Name = "student1"; 
+            user.Email = "student1@asdas.com";
+            user.Password = "123456";
+            
+             //save student object in connection 
+             await connection.manager.save(user);
+             console.log("Saved a new user with id: " + user.id);
+             
+             console.log("Loading users from the database...");
+         
+             //Display student saved records
+             const users = await connection.manager.find(User);
+             console.log("Loaded users: ", users);
+         
+             console.log("Here you can setup and run express/koa/any other framework.");
+
+             res.send(users);
+         }).catch(error => console.log(error));
     }
 }
