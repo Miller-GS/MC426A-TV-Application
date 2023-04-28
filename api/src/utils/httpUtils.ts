974bcbc5658003c9d3d59702e42e167b7fe96306
Gabrielle Barbosa <gabrielle.gabi.barbosa@gmail.com>
2023-04-27 22:46:13 -0300

    :white_check_mark: Http utils tests
    Co-authored-by: Vinschers <github.k9qcx@slmail.me>

--- api/src/utils/httpUtils.ts
+++ api/src/utils/httpUtils.ts
@@ -1,6 +1,11 @@
+import { ValidationUtils } from "./validationUtils";
+
 export class HttpUtils {
     static buildQuery(obj) {
+        if(obj == undefined) return ""
+        
         return Object.keys(obj)
+            .filter((key) => !ValidationUtils.isEmpty(obj[key]))
             .map((key) => {
                 return `${key}=${encodeURIComponent(obj[key])}`;
             })
--- api/tests/utils/httpUtils.test.ts
+++ api/tests/utils/httpUtils.test.ts
@@ -1,7 +1,28 @@
+import { HttpUtils } from "../../src/utils/httpUtils";
 
-describe("Http Utils", () => {
-    test("list controller", async () => {
-        const response = "";
-        expect(response).toEqual("Hello00o, World!");
+describe("buildQuery", () => {
+    test("Query with no arguments", async () => {
+        const response = HttpUtils.buildQuery({});
+        expect(response).toEqual("");
+    });
+
+    test("Query with undefined object", async () => {
+        const response = HttpUtils.buildQuery(undefined);
+        expect(response).toEqual("");
+    });
+
+    test("Query with object with 1 key", async () => {
+        const response = HttpUtils.buildQuery({key: "value"});
+        expect(response).toEqual("key=value");
+    });
+
+    test("Query with object with multiple keys", async () => {
+        const response = HttpUtils.buildQuery({key1: "value1", key2: "value2"});
+        expect(response).toEqual("key1=value1&key2=value2");
+    });
+
+    test("Query with object with special characters", async () => {
+        const response = HttpUtils.buildQuery({key1: " ", key2: "?", key3: "&", key4: "", key5: undefined});
+        expect(response).toEqual("key1=%20&key2=%3F&key3=%26");
     });
 });
