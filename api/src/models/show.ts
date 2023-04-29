ac869affb07c6089a2dd5591aeeac64f066bc8be
Felipe Scherer Vicentin <github.k9qcx@slmail.me>
2023-04-29 17:52:20 -0300

    :white_check_mark: Add unit tests for Show model
    Co-authored-by: GabrielleBarbosa <gabrielle.gabi.barbosa@gmail.com>

--- api/src/models/show.ts
+++ api/src/models/show.ts
@@ -1,4 +1,5 @@
 import env from "../../environment";
+import { ValidationUtils } from "../utils/validationUtils";
 
 export class ShowTypeEnum {
     public static TV: string = "TV";
@@ -9,6 +10,7 @@ export interface Show {
     id: number;
     name: string;
     genres: number[];
+    showType: string;
 
     description: string;
     language: string;
@@ -24,6 +26,8 @@ export interface Show {
 
 export class ShowParser {
     static parseTv(tvObj: any): Show {
+        if (tvObj === undefined) return {} as Show;
+
         const show = {
             id: tvObj.id,
             name: tvObj.original_title,
@@ -31,18 +35,29 @@ export class ShowParser {
             description: tvObj.overview,
             language: tvObj.original_language,
             releaseDate: tvObj.first_air_date,
-            backgroundImageUrl: `${env.TMDB_IMAGE_URL}${tvObj.backdrop_path}`,
-            posterImageUrl: `${env.TMDB_IMAGE_URL}${tvObj.poster_path}`,
             popularity: tvObj.popularity,
             voteAverage: tvObj.vote_average,
             voteCount: tvObj.vote_count,
             showType: ShowTypeEnum.TV,
-        };
+        } as Show;
+
+        Object.keys(show).forEach(
+            (key) => show[key] === undefined && delete show[key]
+        );
+
+        if (!ValidationUtils.isEmpty(tvObj.backdrop_path)) {
+            show.backgroundImageUrl = `${env.TMDB_IMAGE_URL}${tvObj.backdrop_path}`;
+        }
+        if (!ValidationUtils.isEmpty(tvObj.poster_path)) {
+            show.posterImageUrl = `${env.TMDB_IMAGE_URL}${tvObj.poster_path}`;
+        }
 
         return show;
     }
 
     static parseMovie(movieObj: any): Show {
+        if (movieObj === undefined) return {} as Show;
+
         const show = {
             id: movieObj.id,
             name: movieObj.original_title,
@@ -50,13 +65,22 @@ export class ShowParser {
             description: movieObj.overview,
             language: movieObj.original_language,
             releaseDate: movieObj.release_date,
-            backgroundImageUrl: `${env.TMDB_IMAGE_URL}${movieObj.backdrop_path}`,
-            posterImageUrl: `${env.TMDB_IMAGE_URL}${movieObj.poster_path}`,
             popularity: movieObj.popularity,
             voteAverage: movieObj.vote_average,
             voteCount: movieObj.vote_count,
             showType: ShowTypeEnum.MOVIE,
-        };
+        } as Show;
+
+        if (!ValidationUtils.isEmpty(movieObj.backdrop_path)) {
+            show.backgroundImageUrl = `${env.TMDB_IMAGE_URL}${movieObj.backdrop_path}`;
+        }
+        if (!ValidationUtils.isEmpty(movieObj.poster_path)) {
+            show.posterImageUrl = `${env.TMDB_IMAGE_URL}${movieObj.poster_path}`;
+        }
+
+        Object.keys(show).forEach(
+            (key) => show[key] === undefined && delete show[key]
+        );
 
         return show;
     }
