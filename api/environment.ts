a74dcf72f2ea8388f33d6e309b73863e675d2b1a
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-04-30 18:47:57 -0300

    feat: add DB configs to .env

--- api/environment.ts
+++ api/environment.ts
@@ -6,4 +6,9 @@ export default {
     TMDB_KEY: process.env.TMDB_AUTH_KEY ?? "",
     TMDB_URL: process.env.TMDB_URL ?? "",
     TMDB_IMAGE_URL: process.env.TMDB_IMAGE_URL ?? "",
+    DB_HOST: process.env.DB_HOST ?? "",
+    DB_NAME: process.env.DB_NAME ?? "",
+    DB_PORT: process.env.DB_PORT ?? "",
+    DB_USER: process.env.DB_USER ?? "",
+    DB_PASSWORD: process.env.DB_PASSWORD ?? "",
 };
