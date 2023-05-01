0da40f544390ac86add6f600a9583f243fd542a4
Monica Okamoto Albrecht <monica.albrecht@gmail.com>
2023-04-30 22:41:29 -0300

    add login router

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -4,9 +4,14 @@ import appDataSource from "../config/ormconfig";
 
 export const usersRouter = express.Router();
 
-const controller = new UsersController(appDataSource);
+export const controller = new UsersController(appDataSource);
 
 usersRouter.post(
     "/register",
     async (req, res) => await controller.register(req, res)
+);
+
+usersRouter.post(
+    "/login",
+    async (req, res) => await controller.login(req, res)
 );
