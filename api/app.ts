b937e43f2a27921323924fdeb0400c6cd4b87267
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-05-01 10:41:45 -0300

    feat: use cookieParser

--- api/app.ts
+++ api/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
-import { usersRouter } from "./src/routes/users";
 import "reflect-metadata";
+import { usersRouter } from "./src/routes/users";
 import tvRouter from "./src/routes/tv";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/users", usersRouter);
 app.use("/tv", tvRouter);
