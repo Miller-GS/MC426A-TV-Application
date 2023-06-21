b5d9f12eee09afcef7dea0b610996fbefad8f570
Felipe Scherer Vicentin <vicentin.py@gmail.com>
2023-06-20 21:16:11 -0300

    :bug: Add withDeleted to readNotification query

--- api/src/services/notificationService.ts
+++ api/src/services/notificationService.ts
@@ -29,6 +29,7 @@ export default class NotificationService {
             where: {
                 Id: notificationId,
             },
+            withDeleted: true,
         });
 
         if (!notification) {
