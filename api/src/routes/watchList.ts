b46c024b66558f1ce1f27584e0ab28c266d807bd
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-06-25 22:40:37 -0300

    feat: add watchlist route

---
+++ api/src/routes/watchList.ts
@@ -0,0 +1,28 @@
+import { WatchListController } from "../controllers/watchList";
+import appDataSource from "../config/ormconfig";
+import WatchListService from "../services/watchListService";
+import { UserEntity } from "../entity/user.entity";
+import { WatchListEntity } from "../entity/watchList.entity";
+import { WatchListItemEntity } from "../entity/watchListItem.entity";
+import express from "express";
+import auth from "../middleware/auth";
+
+export const watchListRouter = express.Router();
+
+const userRepository = appDataSource.getRepository(UserEntity);
+const watchListRepository = appDataSource.getRepository(WatchListEntity);
+const watchListItemRepository = appDataSource.getRepository(WatchListItemEntity);
+
+const service = new WatchListService(
+    watchListRepository,
+    watchListItemRepository,
+    userRepository,
+);
+
+const controller = new WatchListController(service);
+
+watchListRouter.post(
+    "/create/",
+    auth,
+    async (req, res) => await controller.createWatchList(req, res)
+);
